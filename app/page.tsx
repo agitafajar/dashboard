3b87bc0fec0648e6845a1dac0a5ce9e8c4d7985b
agityoutap <agita.prabowo@ext.youtap.id>
2025-11-05 11:30:24 +0700

    refactor(auth): remove login route and authorization check
    The login route was deleted as it's no longer needed with the new authentication strategy. The authorization check in the home page was also removed to simplify the initial page load and redirect logic.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { getToken } from "@/services/auth.service";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const router = useRouter();
-  const authorized = typeof window !== "undefined" && !!getToken();
-
-  useEffect(() => {
-    if (!authorized) {
-      router.replace("/login");
-    }
-  }, [authorized, router]);
-
-  if (!authorized) {
-    return null;
-  }
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -25,6 +12,13 @@ export default function Home() {
         <h1 className="text-4xl font-bold text-black dark:text-white">
           Welcome to the Dashboard
         </h1>
+
+        <Button
+          className="mt-8 cursor-pointer"
+          onClick={() => router.replace("/users")}
+        >
+          Go To List User
+        </Button>
         <Button
           className="mt-8 cursor-pointer"
           onClick={() => router.replace("/logout")}
